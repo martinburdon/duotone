016d397ef880eeaccedf033cb3ef8bc58e4e2d34
Martin Burdon <martin@martinburdon.co.uk>
2020-10-11 21:23:14 +0100

    Pass style props

--- components/DuoImage.js
+++ components/DuoImage.js
@@ -2,11 +2,11 @@ import { Flex, Image } from '@chakra-ui/core';
 import styled from '@emotion/styled';
 
 const StyledContainer = styled(Flex)`
-  background: #f32959; // TODO: Config
+  background: ${(props) => props.color1};
   position: relative;
 
   &::after {
-    background-color: #1d3162; // TODO: Config
+    background-color: ${(props) => props.color2};
     content: '';
     height: 100%;
     left: 0;
@@ -18,14 +18,26 @@ const StyledContainer = styled(Flex)`
 `;
 
 const StyledImage = styled(Image)`
-  filter: grayscale(100%) contrast(1);
-  mix-blend-mode: darken; // TODO Config multiply, darken, lighten
+  filter: grayscale(100%) contrast(${(props) => props.contrast});
+  mix-blend-mode: ${(props) =>
+    props.mixBlendMode}; // TODO Config multiply, darken, lighten
 `;
 
-const DuoImage = ({ src }) => {
+const DuoImage = ({
+  src,
+  color1 = '#f32959',
+  color2 = '#1d3162',
+  contrast = 1,
+  mixBlendMode = 'darken'
+}) => {
   return (
-    <StyledContainer justifyContent="center" w="100%">
-      <StyledImage src={src} />
+    <StyledContainer
+      color1={color1}
+      color2={color2}
+      justifyContent="center"
+      w="100%"
+    >
+      <StyledImage contrast={contrast} mixBlendMode={mixBlendMode} src={src} />
     </StyledContainer>
   );
 };
